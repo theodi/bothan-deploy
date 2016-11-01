3a517a11577eca1bc63f6020ca46c186ed29db03
pezholio <pezholio@gmail.com>
2016-11-01 13:38:03 +0000

    Use Rack::Session::Cookie

--- lib/bothan_deploy.rb
+++ lib/bothan_deploy.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'tilt/erubis'
 require 'json'
 require 'heroku/bouncer'
+require 'platform-api'
 
 require_relative 'bothan_deploy/racks'
 require_relative 'bothan_deploy/helpers'
@@ -9,7 +10,7 @@ require_relative 'bothan_deploy/helpers'
 module BothanDeploy
   class App < Sinatra::Base
 
-    #use Rack::Session::Cookie, secret: ENV['BOTHAN_DEPLOY_SESSION_SECRET'], key: 'bothan_deploy_session'
+    use Rack::Session::Cookie, secret: ENV['BOTHAN_DEPLOY_SESSION_SECRET'], key: 'bothan_deploy_session'
     use Heroku::Bouncer,
       oauth: { id: ENV['HEROKU_OAUTH_ID'], secret: ENV['HEROKU_OAUTH_SECRET'], scope: 'write-protected' },
       secret: ENV['HEROKU_BOUNCER_SECRET'], expose_token: true
@@ -19,10 +20,14 @@ module BothanDeploy
     end
 
     get '/' do
-      headers 'Vary' => 'Accept'
+      @title = 'Deploy your own Bothan'
+      erb :index, layout: :default
+    end
 
-      @content = '<h1>Hello from BothanDeploy</h1>'
-      @title = 'BothanDeploy'
+    post '/deploy' do
+      halt(401) unless request.env['bouncer.token']
+      heroku = PlatformAPI.connect_oauth(request.env['bouncer.token'])
+      @app = heroku.app_setup.create({source_blob: {url: 'https://github.com/theodi/bothan/tarball/master'}})
       erb :index, layout: :default
     end
 
